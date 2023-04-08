aa8a89da0d59ce0338f4dbd01aa9324f29917e91
Chris Maki <c.william.developer@gmail.com>
2023-04-08 18:11:41 -0500

    Before packaging

--- renderer.js
+++ renderer.js
@@ -17,3 +17,8 @@ document
 document.getElementById(
   "info"
 ).innerHTML = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`;
+
+document.getElementById(
+    "notes"
+  ).innerHTML = "Notes:"
+  
